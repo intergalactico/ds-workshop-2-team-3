cfaec33bfae033919aa6e3fc2eeaf25804713214
Vili Rantanen <vili.rantanen@gmail.com>
2018-06-12 13:38:56 +0300

    HeaderLink Styles
    123

--- src/HeaderLinks/styles.js
+++ src/HeaderLinks/styles.js
@@ -1,5 +1,8 @@
 import styled, { css } from 'styled-components'
 
 export const HeaderLinks = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: middle;
   color: red;
 `
--- src/HeaderLogo/styles.js
+++ src/HeaderLogo/styles.js
@@ -2,5 +2,7 @@ import styled, { css } from 'styled-components'
 
 export const HeaderLogo = styled.div`
   max-height: 100%;
+  max-width: 50px;
   display: flex;
+  align-content: center;
 `
